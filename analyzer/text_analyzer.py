777508a3662f85974d46a6ae277bb041e1a1b3ea
Jorge Salcedo <jorgesalcedo078@gmail.com>
2026-03-20 21:56:15 -0600

    agrego archivos del proyecto

---
+++ analyzer/text_analyzer.py
@@ -0,0 +1,57 @@
+def count_words(text):
+	return len(text.split())
+
+
+def count_characters(text):
+	return len(text)
+
+
+def count_sentences(text):
+	return text.count(".")
+
+
+def count_paragraphs(text):
+	paragraphs = text.split("\n\n")
+	paragraphs = [p for p in paragraphs if p.strip()]
+	return len(paragraphs)
+
+
+def longest_word(text):
+	words = text.split()
+	if not words:
+		return ""
+	return max(words, key=len)
+
+
+def longest_sentence(text):
+	sentences = text.split(".")
+	sentences = [s for s in sentences if s.strip()]
+	if not sentences:
+		return ""
+	return max(sentences, key=len).strip()
+
+
+def longest_paragraph(text):
+	paragraphs = text.split("\n\n")
+	paragraphs = [p for p in paragraphs if p.strip()]
+	if not paragraphs:
+		return ""
+	return max(paragraphs, key=len).strip()
+def count_paragraphs(text):
+	paragraph=text.split("\n\n")
+	paragraphs=[p for p in paragraph if p.strip()]
+	return len(paragraphs)
+
+def longest_word(text):
+	words = text.split()
+	if not words:
+		return ""
+	return max(words, key=len)
+def longest_sentence(text):
+    sentences = text.split(".")
+    sentences = [s for s in sentences if s.strip()]
+    return max(sentences, key=len).strip()
+def longest_paragraph(text):
+    paragraphs = text.split("\n\n")
+    paragraphs = [p for p in paragraphs if p.strip()]
+    return max(paragraphs, key=len).strip()
---
+++ tests/main.py
@@ -0,0 +1,20 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from analyzer.text_analyzer import (
+	count_words,
+	count_characters,
+	count_sentences,
+	longest_word,longest_sentence,longest_paragraph
+)
+
+
+text = ""
+ #Esta es una prueba
+print("Palabras:", count_words(text))
+print(f"Caracteres:", count_characters(text))
+print(f"Oraciones:", count_sentences(text))
+print(f"Palabra mas larga:", longest_word(text))
+print(f"Oracion mas larga:", longest_sentence(text))
+print(f"Parrafo mas largo:", longest_paragraph(text))
